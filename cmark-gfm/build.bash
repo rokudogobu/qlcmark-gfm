540d5c40dd24a71100f8d2ad62a114e10869dba6
rokudogobu <52468646+rokudogobu@users.noreply.github.com>
2023-04-08 01:35:09 +0900

    udpate: cmark-gfm version to 0.29.0.gfm.11

--- cmark-gfm/build.bash
+++ cmark-gfm/build.bash
@@ -16,7 +16,7 @@
 
 set -e
 
-url=https://github.com/github/cmark-gfm/archive/0.29.0.gfm.0.zip
+url=https://github.com/github/cmark-gfm/archive/0.29.0.gfm.11.zip
 
 [ -n "$CMAKE" ] || CMAKE="$( which cmake || echo )"
 [ -x "$CMAKE" ] || { echo "*** error: 'cmake' not found. you must install it first, and then run this script again." >&2; exit 1; }
